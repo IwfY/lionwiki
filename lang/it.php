39a10a37f6b64df2626ff83b6c239fbcdbdee0b4
adamzivner <adamzivner@a09fbdd0-cf5a-11dd-b6f5-21ad4398c266>
2008-12-21 18:34:49 +0000

    Italian translation for 2.1
    git-svn-id: http://lionwiki.googlecode.com/svn/trunk@5 a09fbdd0-cf5a-11dd-b6f5-21ad4398c266

---
+++ lang/it.php
@@ -0,0 +1,29 @@
+<?
+		
+	$T_HOME = "Pagina principale";
+	$T_SYNTAX = "Sintassi";
+	$T_EDIT = "Modifica";
+	$T_DONE = "Salva modifiche";
+	$T_PREVIEW = "Anteprima";
+	$T_SEARCH = "Cerca";
+	$T_SEARCH_RESULTS = "Risultati della ricerca";
+	$T_LIST_OF_ALL_PAGES = "Elenco di tutte le pagine";
+	$T_RECENT_CHANGES = "Modifiche recenti";
+	$T_LAST_CHANGED = "Ultime modifiche";
+	$T_HISTORY = "Cronologia";
+	$T_NO_HISTORY = "Cronologia non presente.";
+	$T_RESTORE = "Ripristina";
+	$T_REV_DIFF = "<b>Differenze tra le revisioni a partire dalla {REVISION1} e dalla {REVISION2}.</b>";
+	$T_REVISION = "'''Questa revisione risale a {TIME}. Potete {RESTORE} rla.'''\n";
+	$T_PASSWORD = "Password";
+	$T_EDIT_SUMMARY = "Modifica indice";
+	$T_ERASE_COOKIE = "Cancella i cookies";
+	$T_WIKI_CODE = "Codice del Wiki";
+	$T_MOVE_TEXT = "Nuovo nome";
+	$T_MOVE = "Sposta";
+	$T_DIFF = "diff";
+	$T_CREATE_PAGE = "Crea pagina";
+	$T_PROTECTED_READ = "Dovete inserire una password per visualizzare il contenuto del sito: ";
+	$T_EDIT_CONFLICT = "Conflitto di modifica: qualcuno ha salvato questa pagina dopo che avete iniziato a modificarla. È caldamente consigliato di visualizzare le ultime {DIFF} prima di salvarla. Dopo la revisione e la possibile unione delle modifiche, potete salvare la pagina cliccando sul pulsante Salva.";
+	$TE_WRONG_PASSWORD = "La password non è corretta.";
+?>
