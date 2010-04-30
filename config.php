997e36380530ad642a6c63aed51f4a3209671309
adam.zivner <adam.zivner@a09fbdd0-cf5a-11dd-b6f5-21ad4398c266>
2010-04-30 14:48:29 +0000

    git-svn-id: http://lionwiki.googlecode.com/svn/trunk@137 a09fbdd0-cf5a-11dd-b6f5-21ad4398c266

--- config.php
+++ config.php
@@ -2,12 +2,12 @@
 $WIKI_TITLE = 'My new wiki'; // name of the site
 
 // SHA1 hash of password. If empty (or commented out), no password is required
-$PASSWORD = sha1("aaa");
+$PASSWORD = sha1("my_password");
 
 $TEMPLATE = 'templates/dandelion.html'; // presentation template
 
 // if true, you need to fill password for reading pages too
 // before setting to true, read http://lionwiki.0o.cz/index.php?page=UserGuide%3A+How+to+use+PROTECTED_READ
-$PROTECTED_READ = true;
+$PROTECTED_READ = false;
 
 $NO_HTML = false; // XSS protection
