e993e089ba566d3b3b37bf599c2b1bf4568bc522
adam.zivner <adam.zivner@a09fbdd0-cf5a-11dd-b6f5-21ad4398c266>
2010-09-20 18:36:05 +0000

    git-svn-id: http://lionwiki.googlecode.com/svn/trunk@144 a09fbdd0-cf5a-11dd-b6f5-21ad4398c266

--- lang/de.php
+++ lang/de.php
@@ -1,6 +1,6 @@
 <?php
 $T_HOME = 'Hauptseite';
-$T_EDIT = 'Editieren';
+$T_EDIT = 'Bearbeiten';
 $T_DONE = 'Speichern';
 $T_DISCARD_CHANGES = 'Änderungen verwerfen';
 $T_PREVIEW = 'Vorschau';
@@ -14,7 +14,6 @@ $T_RESTORE = 'Wiederherstellen';
 $T_REV_DIFF = '<b>Unterschied zwischen Revision von {REVISION1} und {REVISION2}.</b>';
 $T_REVISION = "'''Diese Revision ist vom {TIME}. Du kannst sie {RESTORE}.'''\n\n";
 $T_PASSWORD = 'Passwort';
-$T_EDIT = 'Bearbeiten';
 $T_EDIT_SUMMARY = 'Zusammenfassung der Bearbeitung';
 $T_EDIT_CONFLICT = 'Konflikt: Jemand anderes hat diese Seite gespeichert, nachdem Du mit dem Bearbeiten begonnen hast. Überprüfe diese Änderungen und übernehme sie ggf., bevor Du mit dem Speichern fortfährst.';
 $T_SHOW_SOURCE = 'Quelltext anzeigen';
