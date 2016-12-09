b23cd5d03688fbdf93d99f4fc88688df4f65316a
Quent-in <pages_quentin@hotmail.com>
2016-12-09 18:54:42 +0100

    Occitan locale file

---
+++ lang/oc.php
@@ -0,0 +1,29 @@
+<?php
+/*
+	Occitan version, lengadocian dialecte, also called General Occitan.
+*/
+$T_HOME = 'Acuèlh';
+$T_SYNTAX = 'Ajuda (sintaxi)';
+$T_EDIT = 'Modificar';
+$T_DONE = 'Enregistrar';
+$T_PREVIEW = 'Previsualizar';
+$T_SEARCH = 'Recercar';
+$T_SEARCH_RESULTS = 'Resultats de recèrca per';
+$T_LIST_OF_ALL_PAGES = 'Lista de las paginas';
+$T_RECENT_CHANGES = 'Cambiament recents';
+$T_LAST_CHANGED = 'Darrièra modificacion';
+$T_HISTORY = 'Istoric';
+$T_RESTORE = 'Restaurar';
+$T_REV_DIFF = '<b>Diferéncia entre las versions {REVISION1} e {REVISION2}.</b>';
+$T_REVISION = "'''Aquesta version es del {TIME}. Podètz la {RESTORE}.'''\n\n";
+$T_PASSWORD = 'Senhal';
+$T_EDIT_SUMMARY = 'Resumit de las modificacions :';
+$T_ERASE_COOKIE = 'Esfacar los cookies';
+$T_SHOW_SOURCE = 'Mostrar lo còdi font';
+$T_SHOW_PAGE = 'Mostrar la pagina';
+$T_MOVE_TEXT = 'Nòu nom';
+$T_DIFF = 'Comparar';
+$T_CREATE_PAGE = 'Crear una pagina';
+$T_PROTECTED_READ = 'Vos cal picar lo senhal per veire lo contengut : ';
+$T_EDIT_CONFLICT = 'Conflicte : qualqu\'un mai a enregistrat aquesta pagina abans que l\'ajatz enregistrada. Es recomandat de gaitar la darrièra modificacion {DIFF} abans d\'enregistrar la pagina. Aprèp aver trobat, comparat e juntat las modificacions simultanèas, poiretz enregistrar la papgina en clicant sul boton <i>enregistrar</i>.';
+$T_WRONG_PASSWORD = $T_PASSWORD . 'Lo senhal es incorrècte.';
